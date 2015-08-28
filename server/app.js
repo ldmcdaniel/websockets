d58f9c89666fdbddbdf8d590a24d8ee79b79ce14
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-28 00:23:31 -0500

    working apps

--- server/app.js
+++ server/app.js
@@ -11,4 +11,19 @@ var server = app.listen(3000, function () {
   console.log('Server listening on port 3000');
 });
 
-socketio(server);
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+  console.log('Client connected:', socket.id);
+
+  socket.on('disconnect', function () {
+    console.log('Client disconnected:', socket.id);
+  });
+
+  socket.on('sentChatMessage', function (msg) {
+    // socket.emit('chatMessageToOne', msg);
+    // socket.broadcast.emit('chatMessageToOthers', msg);
+    // io.emit('chatMessageToAll', msg});
+    io.emit('recievedChatMessage', msg);
+  });
+});
